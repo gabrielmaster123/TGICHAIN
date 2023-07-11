c3ec3b058ffad7affb55dec0ad219e5b49410849
gabrielmaster123 <gabrielmalbertin@gmail.com>
2023-07-11 11:04:18 +0200

    Added first testing

--- Block.java
+++ Block.java
@@ -7,11 +7,28 @@ public class Block {
     public String hash;
     public String prevHash;
     private String data;
-    private String timeStamp;
+    private long timeStamp;
+
+    
+    
 
     public Block(String data, String prevHash){
         this.data = data;
         this.prevHash = prevHash;
         this.timeStamp = new Date().getTime();
+        this.hash = calculateHash();
+    }
+
+    public String calculateHash() {
+        String calculatedhash = StringUtil.applySha256( 
+                prevHash +
+                Long.toString(timeStamp) +
+                data 
+                );
+    return calculatedhash;
     }
 }
+
+
+
+
