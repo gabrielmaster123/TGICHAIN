b19d5f26d64613b0f76101b4d4644a68b8e7e00c
gabrielmaster123 <gabrielmalbertin@gmail.com>
2023-07-11 11:26:43 +0200

    First Working Blockchain

--- Block.java
+++ Block.java
@@ -8,14 +8,13 @@ public class Block {
     public String prevHash;
     private String data;
     private long timeStamp;
-
-    
-    
+    private int nonce;
 
     public Block(String data, String prevHash){
         this.data = data;
         this.prevHash = prevHash;
         this.timeStamp = new Date().getTime();
+
         this.hash = calculateHash();
     }
 
@@ -23,10 +22,20 @@ public class Block {
         String calculatedhash = StringUtil.applySha256( 
                 prevHash +
                 Long.toString(timeStamp) +
+                Integer.toString(nonce) +
                 data 
                 );
     return calculatedhash;
     }
+
+    public void mineBlock(int difficulty) {
+		String target = new String(new char[difficulty]).replace('\0', '0'); //Create a string with difficulty * "0" 
+		while(!hash.substring( 0, difficulty).equals(target)) {
+			nonce ++;
+			hash = calculateHash();
+		}
+		System.out.println("Block Mined!!! : " + hash);
+	}
 }
 
 
